70da5cf2402c3e3d9da20651ce63f75b100595a1
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-06-13 19:17:45 +0100

    pruned imports;

--- examples/Hub.cs
+++ examples/Hub.cs
@@ -5,11 +5,7 @@ Nik Sultana, Cambridge University Computer Lab, June 2016
 Use of this source code is governed by the Apache 2.0 license; see LICENSE.
 */
 
-using System;
-using System.Net.NetworkInformation;
-using System.Collections.Generic;
 using PacketDotNet;
-using System.Diagnostics;
 using Pax;
 
 public class Hub : MultiInterface_SimplePacketProcessor {
