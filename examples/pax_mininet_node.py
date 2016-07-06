4863bc8fe9e14dcfac91ad74ecc66d235f44ae2e
Jonny Shipton <tmvector@gmail.com>
2016-07-06 14:59:48 +0100

    Restore original value of ip_forward at terminate
    Not sure that the terminate method is necessary, but best be consistent.

--- examples/pax_mininet_node.py
+++ examples/pax_mininet_node.py
@@ -5,13 +5,13 @@ pax_mininet_node.py: Defines PaxNode which allows Pax to behave as the sole pack
 """
 
 from mininet.node import Node
-from mininet.log import info, warn
 
 class PaxNode( Node ):
     "PaxNode: A node which allows Pax to behave as the sole packet hander on that node."
 
     def __init__(self, name, **params):
         super(PaxNode, self).__init__(name, **params)
+        self.ip_forward = ""
 
     def config(self, **params):
         super(PaxNode, self).config(**params)
@@ -25,11 +25,15 @@ class PaxNode( Node ):
         # Disable ip_forward because otherwise, even with the above iptables rules, the OS
         #  will still forward packets that have a different IP on the other interfaces, which
         #  is not the behaviour we want from an ideal node that only processes packets through Pax.
+        self.ip_forward = self.cmd("sysctl -n net.ipv4.ip_forward")
         self.cmd("sysctl -w net.ipv4.ip_forward=0")
 
     def terminate(self):
         # Remove iptables rules
         for intf in self.intfList():
             self.cmd("iptables -D INPUT -p tcp -i %s -j DROP" % intf.name)
+
+        # Restore ip_forward value
+        self.cmd("sysctl -w net.ipv4.ip_forward=%s" % self.ip_forward)
 
         super(PaxNode, self).terminate()
