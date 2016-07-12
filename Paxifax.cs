41b37f9c7da3677aa33ce199b748c9adf523266d
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-12 07:27:19 +0100

    restructured, to group together the core packet processor definitions and instances;

--- Paxifax.cs
+++ Paxifax.cs
@@ -21,6 +21,7 @@ using System.Threading.Tasks;
 using System.Reflection;
 using System.Linq;
 
+// FIXME use javadoc-style comments to describe the API
 namespace Pax {
 
   // A more abstract interface to packet processors.
@@ -30,130 +31,10 @@ namespace Pax {
     ForwardingDecision process_packet (int in_port, ref Packet packet);
   }
 
-  // FIXME use javadoc-style comments to describe the API
   public interface PacketProcessor : PacketProcessor_Improved {
     void packetHandler (object sender, CaptureEventArgs e);
   }
 
-  internal static class PacketProcessorHelper
-  {
-    public readonly static Type[] AllowedConstructorParameterTypes = new Type[]
-      {
-        typeof(Boolean),  typeof(Byte),   typeof(SByte),  typeof(UInt16),   typeof(Int16),
-        typeof(UInt32),   typeof(Int32),  typeof(UInt64), typeof(Int64),    typeof(Decimal),
-        typeof(Single),   typeof(Double), typeof(String), typeof(DateTime),
-        typeof(TimeSpan), typeof(System.Net.IPAddress), typeof(PhysicalAddress)
-      };
-
-    public static bool IsAllowedConstructorParameterType(Type ty)
-    {
-      // Allow nullable types
-      if (ty.IsGenericType && ty.GetGenericTypeDefinition() == typeof(Nullable<>))
-        ty = Nullable.GetUnderlyingType(ty);
-      return AllowedConstructorParameterTypes.Contains(ty);
-    }
-
-    public static object ConvertConstructorParameter(Type ty, string s)
-    {
-      if (ty == typeof(string))
-        return s;
-      else if (ty == typeof(System.Net.IPAddress))
-        return System.Net.IPAddress.Parse(s);
-      else if (ty == typeof(PhysicalAddress))
-        return PhysicalAddress.Parse(s.ToUpper().Replace(':', '-'));
-      else if (ty == typeof(TimeSpan))
-        return TimeSpan.Parse(s);
-      else
-        // Convert to primitives + DateTime
-        return ((IConvertible)s).ToType(ty, System.Globalization.CultureInfo.CurrentCulture); // NOTE throws InvalidCastException
-    }
-
-    public static string ConstructorString(ConstructorInfo constructor, object[] arguments = null)
-    {
-      IEnumerable<string> parameters;
-      // Get the string representations of the parameters (or arguments if present)
-      if (arguments == null)
-        parameters = constructor.GetParameters()
-                                .Select(p => String.Format("{0}: {1}", p.Name, p.ParameterType.FullName));
-      else
-        parameters = arguments.Select(obj => obj.ToString());
-      // Format nicely so it looks like a constructor
-      return String.Format("{0}({1})", constructor.DeclaringType.Name, String.Join(", ", parameters));
-    }
-
-    public static IEnumerable<Type> GetUsedPaxTypes(Type type)
-    {
-      // Yield implemented Pax interfaces
-      foreach (Type intf in type.GetInterfaces())
-      {
-        if (intf.FullName.StartsWith("Pax."))
-          yield return intf;
-        else
-        {
-          // Check for interfaces lower down
-          foreach (Type subintf in GetUsedPaxTypes(intf))
-            yield return subintf;
-        }
-      }
-
-      // Yield the highest-up extended Pax type
-      type = type.BaseType;
-      while (type != null)
-      {
-        if (type.FullName.StartsWith("Pax."))
-        {
-          yield return type;
-          yield break;
-        }
-        type = type.BaseType;
-      }
-    }
-
-    public static PacketProcessor InstantiatePacketProcessor(Type type, IDictionary<string, string> argsDict)
-    {
-      // Predicate determining if a parameter could be provided
-      Func<ParameterInfo,bool> parameterIsAvailable = param =>
-        argsDict.ContainsKey(param.Name) && IsAllowedConstructorParameterType(param.ParameterType);
-      // Predicate determining if a constructor can be called
-      Func<ConstructorInfo,bool> constructorCanBeCalled = ctor =>
-        ctor.GetParameters().All(parameterIsAvailable);
-
-      // Get the constructors for this type that we could call with the given arguments:
-      var constructors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)
-                             .Where(constructorCanBeCalled);
-
-      // Try to instantiate the type, using the best constructor we can:
-      foreach (var constructor in constructors.OrderByDescending(ctor => ctor.GetParameters().Length))
-      {
-        try
-        {
-          // Get the arguments for the constructor, converted to the proper types
-          var arguments =
-            constructor.GetParameters()
-                       .Select(p => ConvertConstructorParameter(p.ParameterType, argsDict[p.Name]))
-                       .ToArray();
-          // Invoke the constructor, instantiating the type
-#if MOREDEBUG
-          Console.WriteLine("Invoking new {0}", ConstructorString(constructor, arguments));
-#endif
-          PacketProcessor pp = (PacketProcessor)constructor.Invoke(arguments);
-          return pp;
-        }
-        catch (Exception ex) when (ex is InvalidCastException
-                                || ex is FormatException)
-        {
-          // If an exception is thrown, ignore it and try the next best constructor
-          // But log it first:
-          Debug.WriteLine("Constructor failed - {0}:", constructor.ToString());
-          Debug.WriteLine(ex);
-        }
-      }
-
-      // If we reach this point, there were no constructors that we could use
-      Console.WriteLine("No suitable constructor could be found.");
-      return null;
-    }
-  }
 
   // A packet monitor does not output anything onto the network, it simply
   // accumulates state based on what it observes happening on the network.
@@ -301,6 +182,127 @@ namespace Pax {
     }
   }
 
+#region Aux functions
+  internal static class PacketProcessorHelper
+  {
+    public readonly static Type[] AllowedConstructorParameterTypes = new Type[]
+      {
+        typeof(Boolean),  typeof(Byte),   typeof(SByte),  typeof(UInt16),   typeof(Int16),
+        typeof(UInt32),   typeof(Int32),  typeof(UInt64), typeof(Int64),    typeof(Decimal),
+        typeof(Single),   typeof(Double), typeof(String), typeof(DateTime),
+        typeof(TimeSpan), typeof(System.Net.IPAddress), typeof(PhysicalAddress)
+      };
+
+    public static bool IsAllowedConstructorParameterType(Type ty)
+    {
+      // Allow nullable types
+      if (ty.IsGenericType && ty.GetGenericTypeDefinition() == typeof(Nullable<>))
+        ty = Nullable.GetUnderlyingType(ty);
+      return AllowedConstructorParameterTypes.Contains(ty);
+    }
+
+    public static object ConvertConstructorParameter(Type ty, string s)
+    {
+      if (ty == typeof(string))
+        return s;
+      else if (ty == typeof(System.Net.IPAddress))
+        return System.Net.IPAddress.Parse(s);
+      else if (ty == typeof(PhysicalAddress))
+        return PhysicalAddress.Parse(s.ToUpper().Replace(':', '-'));
+      else if (ty == typeof(TimeSpan))
+        return TimeSpan.Parse(s);
+      else
+        // Convert to primitives + DateTime
+        return ((IConvertible)s).ToType(ty, System.Globalization.CultureInfo.CurrentCulture); // NOTE throws InvalidCastException
+    }
+
+    public static string ConstructorString(ConstructorInfo constructor, object[] arguments = null)
+    {
+      IEnumerable<string> parameters;
+      // Get the string representations of the parameters (or arguments if present)
+      if (arguments == null)
+        parameters = constructor.GetParameters()
+                                .Select(p => String.Format("{0}: {1}", p.Name, p.ParameterType.FullName));
+      else
+        parameters = arguments.Select(obj => obj.ToString());
+      // Format nicely so it looks like a constructor
+      return String.Format("{0}({1})", constructor.DeclaringType.Name, String.Join(", ", parameters));
+    }
+
+    public static IEnumerable<Type> GetUsedPaxTypes(Type type)
+    {
+      // Yield implemented Pax interfaces
+      foreach (Type intf in type.GetInterfaces())
+      {
+        if (intf.FullName.StartsWith("Pax."))
+          yield return intf;
+        else
+        {
+          // Check for interfaces lower down
+          foreach (Type subintf in GetUsedPaxTypes(intf))
+            yield return subintf;
+        }
+      }
+
+      // Yield the highest-up extended Pax type
+      type = type.BaseType;
+      while (type != null)
+      {
+        if (type.FullName.StartsWith("Pax."))
+        {
+          yield return type;
+          yield break;
+        }
+        type = type.BaseType;
+      }
+    }
+
+    public static PacketProcessor InstantiatePacketProcessor(Type type, IDictionary<string, string> argsDict)
+    {
+      // Predicate determining if a parameter could be provided
+      Func<ParameterInfo,bool> parameterIsAvailable = param =>
+        argsDict.ContainsKey(param.Name) && IsAllowedConstructorParameterType(param.ParameterType);
+      // Predicate determining if a constructor can be called
+      Func<ConstructorInfo,bool> constructorCanBeCalled = ctor =>
+        ctor.GetParameters().All(parameterIsAvailable);
+
+      // Get the constructors for this type that we could call with the given arguments:
+      var constructors = type.GetConstructors(BindingFlags.Instance | BindingFlags.Public)
+                             .Where(constructorCanBeCalled);
+
+      // Try to instantiate the type, using the best constructor we can:
+      foreach (var constructor in constructors.OrderByDescending(ctor => ctor.GetParameters().Length))
+      {
+        try
+        {
+          // Get the arguments for the constructor, converted to the proper types
+          var arguments =
+            constructor.GetParameters()
+                       .Select(p => ConvertConstructorParameter(p.ParameterType, argsDict[p.Name]))
+                       .ToArray();
+          // Invoke the constructor, instantiating the type
+#if MOREDEBUG
+          Console.WriteLine("Invoking new {0}", ConstructorString(constructor, arguments));
+#endif
+          PacketProcessor pp = (PacketProcessor)constructor.Invoke(arguments);
+          return pp;
+        }
+        catch (Exception ex) when (ex is InvalidCastException
+                                || ex is FormatException)
+        {
+          // If an exception is thrown, ignore it and try the next best constructor
+          // But log it first:
+          Debug.WriteLine("Constructor failed - {0}:", constructor.ToString());
+          Debug.WriteLine(ex);
+        }
+      }
+
+      // If we reach this point, there were no constructors that we could use
+      Console.WriteLine("No suitable constructor could be found.");
+      return null;
+    }
+  }
+
   public static class PacketEncap {
     public static bool Encapsulates (this Packet p, params Type[] encs) {
       if (encs.Length > 0)
@@ -322,4 +324,5 @@ namespace Pax {
       }
     }
   }
+#endregion
 }
