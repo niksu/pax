7b706d68b2ef8845cb14fa588736a10a9e0f0bda
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-12 16:31:46 +0100

    added XML documentation;

--- ForwardingDecision.cs
+++ ForwardingDecision.cs
@@ -7,23 +7,33 @@ Use of this source code is governed by the Apache 2.0 license; see LICENSE.
 
 namespace Pax {
 
-  // A packet processor makes some sort of forwarding decision, in addition
-  // to possibly analysing/modifying the packet (and generating new packets).
-  // The forwarding decision makes clear whether the processor recommends to
-  // * drop the packet (i.e., nothing more is to be done with it)
-  // * forward to a single port
-  // * forward to multiple ports
-  //
-  // NOTE this idiom simulates using algebraic types in C#. It's values are
-  //      analysed using the Visitor pattern.
+  /// <summary>
+  /// A packet processor makes some sort of forwarding decision, in addition
+  /// to possibly analysing/modifying the packet (and generating new packets).
+  /// </summary>
+  /// <remark>
+  /// The forwarding decision makes clear whether the processor recommends to
+  /// * drop the packet (i.e., nothing more is to be done with it)
+  /// * forward to a single port
+  /// * forward to multiple ports
+  ///
+  /// NOTE this idiom simulates using algebraic types in C#. It's values are
+  ///      analysed using the Visitor pattern.
+  /// </remark>
   public abstract class ForwardingDecision {
     private ForwardingDecision() {}
 
     // FIXME can use singleton to avoid multiple allocations of this?
+    /// <summary>
+    /// The forwarding decision to drop the packet.
+    /// </summary>
     public sealed class Drop : ForwardingDecision {
       public Drop () {}
     }
 
+    /// <summary>
+    /// The forwarding decision to forward the packet to a single port.
+    /// </summary>
     public sealed class SinglePortForward : ForwardingDecision {
       public readonly int target_port;
 
@@ -32,6 +42,9 @@ namespace Pax {
       }
     }
 
+    /// <summary>
+    /// The forwarding decision to forward the packet to several ports.
+    /// </summary>
     public sealed class MultiPortForward : ForwardingDecision {
       public readonly int[] target_ports;
 
