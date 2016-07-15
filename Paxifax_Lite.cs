bd057f9bf3557dd73c23369cb6d3007f9ccfdf4d
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-15 23:21:37 +0100

    further simplification: modifying statically-allocated memory makes analysis and interoperation easier for downstream bytecode processors targetting constrained environments such as hardware;

--- Paxifax_Lite.cs
+++ Paxifax_Lite.cs
@@ -8,11 +8,17 @@ Use of this source code is governed by the Apache 2.0 license; see LICENSE.
 // FIXME use javadoc-style comments to describe the API
 namespace Pax {
 
-  // Packet processors.
-  public abstract class BytePacket_Processor {
-    byte[] packet = new byte[PaxConfig_Lite.MAX_PACKET_SIZE];
+  // Packet will be retreivable from here, where it will
+  // be deposited by another party (which depends on where
+  // the packet processor is executing).
+  public static class Packet_Buffer {
+    public static byte[] packet = new byte[PaxConfig_Lite.MAX_PACKET_SIZE];
+    public static long forward = 0; // FIXME might not be right place for this
+  }
 
+  // Packet processors.
+  public interface IBytePacket_Processor {
     // FIXME using "long" type is too arbitrary?
-    abstract public long process_packet (byte in_port);
+    void process_packet (byte in_port);
   }
 }
--- examples/Hub.cs
+++ examples/Hub.cs
@@ -7,24 +7,25 @@ Use of this source code is governed by the Apache 2.0 license; see LICENSE.
 
 using Pax;
 
-//#if !LITE
-//using PacketDotNet;
-//
-//public partial class Hub : MultiInterface_SimplePacketProcessor {
-//  override public ForwardingDecision process_packet (int in_port, ref Packet packet)
-//  {
-//    // Extract the bytes and call the instance of IAbstract_ByteProcessor
-//    byte[] bs = packet.Bytes;
-//// FIXME    return (process_packet (in_port, ref bs));
-//    return (ForwardingDecision.broadcast(in_port));
-//  }
-//}
-//#endif
+#if !LITE
+using PacketDotNet;
 
-public /*partial*/ class Hub : BytePacket_Processor {
-  override public long process_packet (byte in_port)
+public class Hub_PP : MultiInterface_SimplePacketProcessor {
+  override public ForwardingDecision process_packet (int in_port, ref Packet packet)
   {
-    long forward = 0;
+    // Extract the bytes and call the instance of IAbstract_ByteProcessor
+    byte[] bs = packet.Bytes;
+// FIXME    return (process_packet (in_port, ref bs));
+    return (ForwardingDecision.broadcast(in_port));
+    // FIXME simple call Hub.process_packet?
+  }
+}
+#endif
+
+public static class Hub {
+  static public void process_packet (byte in_port)
+  {
+    long forward = Packet_Buffer.forward;
 
     for (int ofs = 0; ofs < PaxConfig_Lite.no_interfaces; ofs++)
     {
@@ -35,6 +36,6 @@ public /*partial*/ class Hub : BytePacket_Processor {
       }
     }
 
-    return forward;
+    Packet_Buffer.forward = forward;
   }
 }
