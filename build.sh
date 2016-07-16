40c105510b7feff9742f95f9fbde9207490f2aac
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-16 18:37:03 +0100

    allowing command-line provision of #define's; since build is quite quick, i'm forcing a clean phase before building;

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # FIXME: create a .sln file that includes both projects
-xbuild Pax_Lite.csproj && \
-xbuild Pax.csproj && \
-xbuild examples/Examples.csproj && \
-xbuild examples/Hub.csproj && \
+xbuild Pax_Lite.csproj /t:Rebuild && \
+xbuild Pax.csproj /t:Rebuild && \
+xbuild examples/Examples.csproj /t:Rebuild && \
+xbuild examples/Hub.csproj /t:Rebuild && \
 echo Success
