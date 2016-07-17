c2f8af364bb0257fab0b37de5cdd7ca389e5339c
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-17 13:58:51 +0100

    updated Hub to work with restricted interface;

--- examples/Hub.cs
+++ examples/Hub.cs
@@ -9,21 +9,48 @@ using Pax;
 
 #if !LITE
 using PacketDotNet;
-
-public class Hub_PP : MultiInterface_SimplePacketProcessor {
-  override public ForwardingDecision process_packet (int in_port, ref Packet packet)
-  {
-    // Extract the bytes and call the instance of IAbstract_ByteProcessor
-    byte[] bs = packet.Bytes;
-// FIXME    return (process_packet (in_port, ref bs));
-    return (ForwardingDecision.broadcast(in_port));
-    // FIXME simple call Hub.process_packet?
-  }
-}
 #endif
 
-public static class Hub {
-  static public void process_packet (byte in_port)
+
+public
+#if LITE
+static
+#endif
+class Hub
+#if !LITE
+ : MultiInterface_SimplePacketProcessor
+#endif
+ {
+#if !LITE
+  override public ForwardingDecision process_packet (int in_port, ref Packet packet)
+  {
+/* NOTE disabled this since this processor doesn't look at packets, only at the in_port.
+    // Extract the bytes and call the instance of IAbstract_ByteProcessor
+    byte[] bs = packet.Bytes;
+*/
+/* FIXME ideally we should be able use a single implementation of the logic.
+         This is possible, but requires some in-progress development.
+         Specifically this conists of a phase that switches between
+         the high-level and lower-level encoding of in_port, packet, and forward.
+         Then we get two runtime options:
+         * the Pax instance can use the Pax and Pax-Lite level logic (the latter
+           by using the en/decoding)
+         * the Pax_Lite instance can only use the low-level encoding since the
+           types for the high-level encoding are not in its universe.
+
+    process_packet (in_port);
+    TODO var forward = decode_forward();
+    return forward;
+*/
+    return (ForwardingDecision.broadcast(in_port));
+  }
+#endif
+
+  public
+#if LITE
+  static
+#endif
+  void process_packet (byte in_port)
   {
     long forward = Packet_Buffer.forward;
 
@@ -37,5 +64,6 @@ public static class Hub {
     }
 
     Packet_Buffer.forward = forward;
+    //Packet_Buffer.forward = 0xFAFF; // FIXME currently using a constant for debugging
   }
 }
