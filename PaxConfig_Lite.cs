66a77d4566d228352dcd345f0a205b384772aa54
Nik Sultana <nik.sultana@cl.cam.ac.uk>
2016-07-17 13:57:39 +0100

    improved comment; restricted variable scope via conditional compilation;

--- PaxConfig_Lite.cs
+++ PaxConfig_Lite.cs
@@ -8,15 +8,24 @@ Use of this source code is governed by the Apache 2.0 license; see LICENSE.
 namespace Pax
 {
   public static class PaxConfig_Lite {
-    // This is the number of interfaces in the configuration file.
+    // This is the number of interfaces that Pax processor instances can
+    // forward to. When running in a full-fledged .NET VM, this indicates the
+    // number of network interfaces in the configuration file. When running
+    // in a constrained environment, this can be set directly by the
+    // system after loading (from some static value, or using a configuration
+    // source that it knows of).
     // This must be greater than 1.
     // Note that no_interfaces may be larger than the number of interfaces to which a packet processor has
     // been attached (i.e., interfaces that have a "lead_handler" defined in the configuration).
     // But this is fine, because there might be interface for which we don't want to process
     // their incoming packets, but we want to be able to forward packets to them nonetheless.
-    public static int no_interfaces;
+    public static int no_interfaces; //FIXME make uint?
 
+//#if LITE
+    // We need static bounds on arrays, and these are
+    // provided by such constants.
     public const uint MAX_PACKET_SIZE = 1500; // Maximum size of a packet in bytes.
     public const uint MAX_INTERFACES = 10; // Maximum number of interfaces we can use.
+//#endif
   }
 }
